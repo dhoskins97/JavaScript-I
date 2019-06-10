b86ac5d00128c8a41c717ff236e01124d1f7e0f4
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-06-10 13:05:11 -0400

    halfway complete, awaiting assistance

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,35 @@
 // };
 // myFunction();
 
+let myFunction = function () => console.log("Function was invoked!")
+ myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = function (param) => return param
+
+anotherFunction("Example")
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = function (param1, param2) => return param1 + param2
+
+add(1, 2)
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = function (param1, param2) => return param1 - param2
+
+subtract (1, 2)
 
 // Stretch
 
